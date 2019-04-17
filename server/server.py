4d936e9817b2c125b6069fcc45d75ad853c97efc
ナッティ <hello@nutty.cc>
2019-04-17 13:00:47 -0700

    Make python server port configurable

--- server/server.py
+++ server/server.py
@@ -1,11 +1,23 @@
 from flask import Flask
+import argparse
 
 app = Flask(__name__)
 
 @app.route("/")
 def tissue():
-    return "tissue -- a tiny issue tracker server"
+    return "tissue -- a tiny issue tracker server\n"
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    parser = argparse.ArgumentParser(
+        description="tissue: a tiny issue tracker server"
+    )
+    parser.add_argument(
+        "--port",
+        "-p",
+        type=int,
+        help="http server port",
+        default=5000
+    )
+    args = parser.parse_args()
+    app.run(debug=True, host="0.0.0.0", port=args.port)
 
