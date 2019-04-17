23ba69eb89ad208ab6d5d2c9836a3e0bca0c1817
ナッティ <hello@nutty.cc>
2019-04-17 13:19:51 -0700

    Create API structure

--- server/server.py
+++ server/server.py
@@ -7,6 +7,30 @@ app = Flask(__name__)
 def tissue():
     return "tissue -- a tiny issue tracker server\n"
 
+@app.route("/api")
+def api():
+    return "Not implemented.", 501
+
+@app.route("/api/issue/<int:id>", methods=["GET"])
+def get_issue(id):
+    return "Not implemented.", 501
+
+@app.route("/api/issue/<int:id>", methods=["POST"])
+def create_issue(id):
+    return "Not implemented.", 501
+
+@app.route("/api/issue/<int:id>", methods=["PUT"])
+def replace_issue(id):
+    return "Not implemented.", 501
+
+@app.route("/api/issue/<int:id>", methods=["PATCH"])
+def update_issue(id):
+    return "Not implemented.", 501
+
+@app.route("/api/issue/<int:id>", methods=["DELETE"])
+def delete_issue(id):
+    return "Not implemented.", 501
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="tissue: a tiny issue tracker server"
