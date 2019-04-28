3fa2a5434f7f204834c564f1111a7d0a95b3e329
ナッティ <hello@nutty.cc>
2019-04-27 17:51:53 -0700

    Write tests for `create_issue`

--- server/server_test.py
+++ server/server_test.py
@@ -1,9 +1,9 @@
 import functools
-import pytest
 import sqlite3
 
 from server import (
     SCHEMA_FILE,
+    create_issue,
     fetch_issue,
 )
 
@@ -95,6 +95,7 @@ def test_fetch_issue():
             "expected": None,
         },
     ]
+
     for test_case in test_cases:
         sqlite_setup, sqlite_teardown = mock(sqlite_fixture)
         mock_connection = sqlite_setup()
@@ -102,4 +103,227 @@ def test_fetch_issue():
         assert actual == test_case["expected"]
         sqlite_teardown()
 
+
+def test_create_issue():
+    test_cases = [
+        {
+            "name": "issue without tags",
+            "issue": {
+                "title": "Scroll a 21 atk bwg",
+                "description": "10% chance? 7 times in a row?",
+            },
+            "expectedIssue": {
+                "title": "Scroll a 21 atk bwg",
+                "description": "10% chance? 7 times in a row?",
+            },
+            "expectedTags": [],
+            "expectedException": False,
+        },
+        {
+            "name": "issue with a single tag",
+            "issue": {
+                "title": "Ride the ship to Orbis",
+                "description": "Watch out for the Crimson Balrog!",
+                "tags": [
+                    {
+                        "namespace": "project",
+                        "predicate": "name",
+                        "value": "Maplestory",
+                    },
+                ],
+            },
+            "expectedIssue": {
+                "title": "Ride the ship to Orbis",
+                "description": "Watch out for the Crimson Balrog!",
+            },
+            "expectedTags": [
+                {
+                    "namespace": "project",
+                    "predicate": "name",
+                    "value": "Maplestory",
+                },
+            ],
+            "expectedException": False,
+        },
+        {
+            "name": "issue with multiple tags",
+            "issue": {
+                "title": "Kill a King Slime",
+                "description": "S> track 20k @@@...",
+                "tags": [
+                    {
+                        "namespace": "project",
+                        "predicate": "name",
+                        "value": "Maplestory",
+                    },
+                    {
+                        "namespace": "party",
+                        "predicate": "quest",
+                        "value": "kpq",
+                    },
+                ],
+            },
+            "expectedIssue": {
+                "title": "Kill a King Slime",
+                "description": "S> track 20k @@@...",
+            },
+            "expectedTags": [
+                {
+                    "namespace": "project",
+                    "predicate": "name",
+                    "value": "Maplestory",
+                },
+                {
+                    "namespace": "party",
+                    "predicate": "quest",
+                    "value": "kpq",
+                },
+            ],
+            "expectedException": False,
+        },
+        {
+            "name": "issue with duplicate tags",
+            "issue": {
+                "title": "Issue with duplicated tags",
+                "description": "This should raise an exception.",
+                "tags": [
+                    {
+                        "namespace": "this is a",
+                        "predicate": "duplicated",
+                        "value": "tag",
+                    },
+                    {
+                        "namespace": "this is a",
+                        "predicate": "duplicated",
+                        "value": "tag",
+                    },
+                ],
+            },
+            "expectedIssue": None,
+            "expectedTags": [],
+            "expectedException": True,
+        },
+        {
+            "name": "issue with invalid tag",
+            "issue": {
+                "title": "Issue with invalid tag",
+                "description": "This should raise an exception.",
+                "tags": [
+                    {
+                        "namespace": "invalid",
+                        "predicate": "tag",
+                        # missing value field
+                    },
+                ],
+            },
+            "expectedIssue": None,
+            "expectedTags": [],
+            "expectedException": True,
+        },
+        {
+            "name": "issue with empty tag",
+            "issue": {
+                "title": "Issue with empty tag values",
+                "description": "This should raise an exception.",
+                "tags": [
+                    {
+                        "namespace": "",
+                        "predicate": "",
+                        "value": "",
+                    },
+                ],
+            },
+            "expectedIssue": None,
+            "expectedTags": [],
+            "expectedException": True,
+        },
+        {
+            "name": "issue without 'title' field",
+            "issue": {
+                "description": "Where is the title?",
+            },
+            "expectedIssue": None,
+            "expectedTags": [],
+            "expectedException": True,
+        },
+        {
+            "name": "issue with empty 'title' field",
+            "issue": {
+                "title": "",
+                "description": "Where is the title?",
+            },
+            "expectedIssue": None,
+            "expectedTags": [],
+            "expectedException": True,
+        },
+        {
+            "name": "issue without 'description' field",
+            "issue": {
+                "title": "Where is the description?",
+            },
+            "expectedIssue": {
+                "title": "Where is the description?",
+                "description": "",
+            },
+            "expectedTags": [],
+            "expectedException": False,
+        },
+        {
+            "name": "issue with empty 'description' field",
+            "issue": {
+                "title": "Where is the description?",
+                "description": "",
+            },
+            "expectedIssue": {
+                "title": "Where is the description?",
+                "description": "",
+            },
+            "expectedTags": [],
+            "expectedException": False,
+        },
+    ]
+
+    for test_case in test_cases:
+        sqlite_setup, sqlite_teardown = mock(sqlite_fixture)
+        mock_connection = sqlite_setup()
+
+        try:
+            cursor = mock_connection.cursor()
+            issue_id = create_issue(cursor, test_case["issue"])
+        except Exception:
+            assert test_case["expectedException"]
+            continue
+        assert not test_case["expectedException"]
+
+        # Check that the issue was created.
+        cursor.execute(f"""
+            SELECT *
+            FROM issue
+            WHERE id = ?
+        """, (issue_id,))
+
+        results = cursor.fetchall()
+        assert len(results) == 1
+        assert results[0]["title"] == test_case["expectedIssue"]["title"]
+        assert results[0]["description"] == test_case["expectedIssue"]["description"]
+
+        # Check that the tags were created.
+        for tag in test_case["expectedTags"]:
+            cursor.execute(f"""
+                SELECT *
+                FROM tag
+                WHERE
+                    issue_id = ? AND
+                    namespace = ? AND
+                    predicate = ? AND
+                    value = ?
+            """, (
+                issue_id,
+                tag["namespace"],
+                tag["predicate"],
+                tag["value"],
+            ))
+            assert len(cursor.fetchall()) == 1
+
+        sqlite_teardown()
 
